580f2e028d2468a9caf96445104fe812007ae925
Alexandra <alexandrafernandezquispe@gmail.com>
2016-09-16 14:15:17 -0500

    subiendo bicis

--- js/main.js
+++ js/main.js
@@ -1,85 +1,42 @@
 function validateForm(){
     var nombre = document.getElementById("name").value;
+        if(nombre == ""){
+            var hermano = document.getElementById("name");
+            var texto = document.createTextNode("Debe ingresar su nombre");
+            mensaje(hermano,texto);
+        }
     var apellido = document.getElementById("lastname").value;
-	var email = document.getElementById("input-email").value;
-	var contraseña = document.getElementById("input-password").value;
-    var tipo = document.querySelector("select").value;
-
-    if(nombre == null || nombre.length == 0 || /^\s+$/.test(nombre)){
-        var span = document.createElement("span");
-        var referencia= document.getElementById("name");
-        var padre=referencia.parentNode;
-        padre.insertBefore(span,referencia);
-        var texto = document.createTextNode("Debes ingresar tu nombre");
-        span.appendChild(texto);
-        return span;
-     }else {
-       var m = /^[a-zA-Z]*$/;
-      if(!nombre.search(m)) {
-            console.log("letra")
-            m = /[a-z]/g;
-       }    
-       if(!nombre.search(m)){
-            var span = document.createElement("span");
-            var referencia= document.getElementById("name");
-            var padre=referencia.parentNode;
-            padre.insertBefore(span,referencia);
-            var texto = document.createTextNode("La primera letra debe empezar con maýuscula");
-            span.appendChild(texto);
-            return span;
+       if(apellido == "") {
+          var hermano = document.getElementById("lastname");
+          var texto = document.createTextNode("Debe ingresar su apellido");
+          mensaje(hermano,texto);
         }
-     }
-
-     if(apellido == null || apellido.length == 0 || /^\s+$/.test(apellido)) {
-        var span = document.createElement("span");
-        var referencia= document.getElementById("lastname");
-        var padre=referencia.parentNode;
-        padre.insertBefore(span,referencia);
-        var texto = document.createTextNode("Debes ingresar tu apellido");
-        span.appendChild(texto);
-        return span;
-      } else {
-       var n = /^[a-zA-Z]*$/;
-       if(!apellido.search(n)) {
-            console.log("letra")
-            n = /[a-z]/g;
-        }    
-        if(!apellido.search(n)){
-            var span = document.createElement("span");
-            var referencia= document.getElementById("lastname");
-            var padre=referencia.parentNode;
-            padre.insertBefore(span,referencia);
-            var texto = document.createTextNode("La primera letra debe empezar con maýuscula");
-            span.appendChild(texto);
-            return span;
-        }
-    }
-
-    if(!(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(email))) {
-        var span = document.createElement("span");
-        var referencia= document.getElementById("input-email");
-        var padre=referencia.parentNode;
-        padre.insertBefore(span,referencia);
+    var email = document.getElementById("input-email").value;
+    var expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if ( !expr.test(email) ){
+        var hermano = document.getElementById("input-email");
         var texto = document.createTextNode("Verifique su email");
-        span.appendChild(texto);
-        return span;
-    } else if( contraseña == null || contraseña.length <= 6 || contraseña == "password" || contraseña == 123456 || contraseña == 098754 || /^\s+$/.test(contraseña) ) {
-        var span = document.createElement("span");
-        var referencia= document.getElementById("input-password");
-        var padre=referencia.parentNode;
-        padre.insertBefore(span,referencia);
-        var texto = document.createTextNode("Ingresa una contraseña válida");
-        span.appendChild(texto);
-        return span;
-    } 
-
-    if (tipo == 0) {
-        var span = document.createElement("span");
-        var referencia= document.querySelector("select");
-        var padre=referencia.parentNode;
-        padre.insertBefore(span,referencia);
-        var texto = document.createTextNode("Selecciona un tipo de bici");
-        span.appendChild(texto);
-        return span;
+        mensaje(hermano, texto);
+    }
+    var contra = document.getElementById("input-password").value;
+        if(contra.length < 5 ){
+            var hermano = document.getElementById("input-password");
+            var texto = document.createTextNode("La contraseña debe tener al menos 6 caracteres");
+            mensaje(hermano, texto);
+        }else if(contra == "123456" || contra =="password" || contra == "098754"){
+            alert("Por favor escriba otra contraseña");
         }
+    if (document.getElementsByTagName("select")[0].value == 0){
+        var hermano = document.getElementsByTagName("select")[0];
+        var texto = document.createTextNode("Debes seleccionar al menos un tipo de bici");
+        mensaje(hermano, texto);
+
+    }
+}
+function mensaje (hermano, texto){
+    var mensajeNegro = document.createElement("span");
+    var padreMensaje = hermano.parentNode;
+    padreMensaje.insertBefore(mensajeNegro,hermano);
+    mensajeNegro.appendChild(texto);
+    return mensajeNegro;
 }
